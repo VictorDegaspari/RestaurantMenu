68cfbf550dc3c9e648fea2d076cf35b7d44230be
Victor Degaspari <victor.degaspari@clinicarx.com.br>
2022-10-25 14:44:59 -0300

    readonly input

--- src/components/CounterInput.vue
+++ src/components/CounterInput.vue
@@ -4,7 +4,7 @@
             <button @click="count > 0 ? count-- : count = 0; list.removeItem(props.obj?.id)" class=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
             <span class="m-auto text-2xl font-thin">−</span>
             </button>
-            <input type="text" disabled class="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none" name="custom-input-number" :value="count" />
+            <input type="text" readonly class="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none" name="custom-input-number" :value="count" />
             <button 
                 @click="count++, list.addItem(props.obj)" 
                 class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer"
